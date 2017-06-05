06a9a30d8b8723c9ce18eb276b02952ebf3530f6
MykolaMalius <mmalyus@mail.ru>
2017-06-05 13:18:11 +0300

    Hibernate connection to MySQl

---
+++ Lesson1/src/main/java/Hibernate/Lesson1/Main.java
@@ -0,0 +1,11 @@
+package Hibernate.Lesson1;
+
+import org.hibernate.SessionFactory;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SessionFactory sessionFactory = HibernateUtil.getSessionfactory();
+	}
+}
