78def4f3c6d3c7f5ede6833b2a74836f29ffcc37
MykolaMalius <mmalyus@mail.ru>
2017-06-06 17:50:36 +0300

    Hibernate, JPA mapping trow annotations

---
+++ Lesson5/src/main/java/Hibernate/Lesson5/Main.java
@@ -0,0 +1,11 @@
+package Hibernate.Lesson5;
+
+import org.hibernate.SessionFactory;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+	}
+}
